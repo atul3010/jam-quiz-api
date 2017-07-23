173f5a13d18bdf63931a00c5e8dbd0c1ec936c9e
Atul <atulky3010@gmail.com>
2017-07-23 22:20:30 +0530

    Add basic application controller methods

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      send_success_response(user)
+    else
+      send_error_response(user.errors.full_messages)
+    end
+  end  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,:first_name, :last_name)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  def send_success_response(data = {}, status = :ok)
+    render status: status, json: { status: :success, data: data }
+  end
+
+  def send_failure_response(data = {}, status = :bad_request)
+    render status: status, json: { status: :fail, data: data }
+  end
+
+  def send_error_response(error, status = :unauthorized)
+    render status: status, json: { status: :error, message: error }
+  end
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
